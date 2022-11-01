9bf462f7f9858f4bd77a589b838e63146992c755
Olzhas Suleimenov <olzhassuleimenov@Olzhass-MacBook-Air.local>
2022-11-01 22:10:33 +0600

    Handle auth session, add database

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -5,15 +5,30 @@
 //  Created by Olzhas Suleimenov on 25.10.2022.
 //
 
+import FirebaseAuth
 import UIKit
 
 class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+        
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        validateAuth()
+    }
+    
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen // because by default it pops up and can be swiped down
+            present(nav, animated: false) // animated: false to almost not see ConversationsViewController before login screen pops up
+        }
+    }
 }
 
---
+++ Messenger/Resources/DatabaseManager.swift
@@ -0,0 +1,54 @@
+//
+//  DatabaseManager.swift
+//  Messenger
+//
+//  Created by Olzhas Suleimenov on 01.11.2022.
+//
+
+import Foundation
+import FirebaseDatabase
+
+// Creating public API (functions) that allow us to seamlessly perform operations on database regardless which view controller we are in
+
+final class DatabaseManager {
+    
+    static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+    
+    
+}
+
+// MARK: - Account Management
+
+extension DatabaseManager {
+    
+    // completions block because function to get data out of database is asynchronous
+    public func doesUserExists(with email: String, completion: @escaping (Bool) -> Void) {
+        // get data from database
+        database.child(email).observeSingleEvent(of: .value) { snapshot in
+            // if we able to assign foundEmail it means this email exists already
+            guard snapshot.value as? String != nil else {
+                completion(false)
+                return
+            }
+            
+            completion(true)
+        }
+    }
+    
+    /// Inserts new user to database
+    public func insertUser(with user: ChatAppUser) {
+        database.child(user.emailAddress).setValue([
+            "first_name": user.firstName,
+            "last_name": user.lastName
+        ])
+    }
+}
+
+struct ChatAppUser {
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+    //let profilePictureURL: String
+}
